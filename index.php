629e6472f083b5e4900d52fdc61a10bc6bbb5681
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2023-10-05 20:15:35 -0300

    Get structure from product

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php include 'functions.php';?>
 
 <?php
-  $response = sendRequest('https://api.tiny.com.br/api2/info.php');
-  pre(json_decode($response));
-
-  $numero = '22';
-  $data = "numero=$numero";
-  $response = sendRequest('https://api.tiny.com.br/api2/pedidos.pesquisa.php', $data);
-  pre(json_decode($response));
-
-  $id = '698787824';
+  $id = '735217815';
   $data = "id=$id";
   $response = sendRequest('https://api.tiny.com.br/api2/pedido.obter.php', $data);
+  pre(json_decode($response));
+
+  $id = '735217191';
+  $data = "id=$id";
+  $response = sendRequest('https://api.tiny.com.br/api2/produto.obter.estrutura.php', $data);
+  pre(json_decode($response));
+
+  $id = '735217195';
+  $data = "id=$id";
+  $response = sendRequest('https://api.tiny.com.br/api2/produto.obter.estrutura.php', $data);
   pre(json_decode($response));
 ?>
