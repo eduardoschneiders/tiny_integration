cbbf9921a9d4ab701c5408374ca8f2d79e97db54
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2023-10-05 23:24:28 -0300

    Implement caching for requests

--- functions.php
+++ functions.php
@@ -1,10 +1,20 @@
 <?php
   function sendRequest($url, $data = null, $optional_headers = null) {
+
+    $host = explode('/', $url);
+    $domain = end($host);
+
+    $cache_file = "cache_files/{$domain}?{$data}";
+
+    if(is_file($cache_file)){
+      return json_decode(file_get_contents($cache_file));
+    }
+
     $token = getenv('TOKEN');
 
     $params = array('http' => array(
       'method' => 'POST',
-        'content' => "token=$token&formato=JSON&".$data
+      'content' => "token=$token&formato=JSON&".$data
     ));
 
     if ($optional_headers !== null) {
@@ -20,8 +30,14 @@
 
     $response = @stream_get_contents($fp);
 
+    // pre($http_response_header);
+
     if ($response === false) {
       throw new Exception("Problema obtendo retorno de $url, $php_errormsg");
+    }
+
+    if(json_decode($response)->retorno->status_processamento == '3'){
+      file_put_contents($cache_file, $response);
     }
 
     return json_decode($response);
