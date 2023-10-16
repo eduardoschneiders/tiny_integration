cdad68b8cf077573ef5b36e8e5f5e2393642338e
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2023-10-16 15:06:58 -0300

    Working talão

--- talao.php
+++ talao.php
@@ -14,53 +14,119 @@
     die();
   }
 
-  $grouped_items = [];
+  $grouped_products = [];
 
   foreach ($response->retorno->pedido->itens as $item) {
     $cleaned_name = preg_replace('/ -\s+\d+\/\d+/', '', $item->item->descricao);
 
-    if (!array_key_exists($cleaned_name, $grouped_items)){
-      $grouped_items[$cleaned_name] = [];
+    if (!array_key_exists($cleaned_name, $grouped_products)){
+      $grouped_products[$cleaned_name] = [];
     }
 
-    array_push($grouped_items[$cleaned_name], $item);
+    array_push($grouped_products[$cleaned_name], $item);
   }
 ?>
 
+<table class="table align-middle">
+  <tr>
+    <th>Número do Pedido</th>
+    <th>Cliente</th>
+    <th>Data do Pedido</th>
+    <th>Previsão</th>
+  </tr>
+  <tr>
+    <td><?= $response->retorno->pedido->numero ?></td>
+    <td><?= $response->retorno->pedido->cliente->nome ?></td>
+    <td><?= $response->retorno->pedido->data_pedido ?></td>
+    <td><?= $response->retorno->pedido->data_prevista ?></td>
+  </tr>
+</table>
+
+
 <?php
-  foreach($grouped_items as $name => $items){
-    echo "<br><br><b>Product: {$name}</b><br>";
+  foreach($grouped_products as $product_name => $products){
+    // pre($products);
     echo "
-      <table class=\"table table-striped\">
+      <table class=\"table table-bordered\">
         <tr>
-          <th>Item</th>
-          <th>Quantidade</th>
+          <th>
+            Referenca:
+          </th>
+
+          <td width=\"700px\">
+            {$product_name}
+          </td>
+
+          <th>
+            Talão
+          </th>
+
+          <td>
+            1/2
+          </td>
         </tr>
+        <tr>
+          <td colspan=\"4\">
+            <table class=\"table table-bordered\">
+              <thead>
+                <tr>
+                  <th>Material </th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                " . components($products) . "
+              </tbody>
+            </table>
+          </td>
+        </tr>
+
+        <tr>
+          <th>Observações:</th>
+          <td colspan=\"3\">Observações</td>
+        </tr>
+      </table>
+
+      <hr class=\"my-5\">
     ";
-
-    foreach($items as $item){
-      $response = api_request('https://api.tiny.com.br/api2/produto.obter.estrutura.php', "id={$item->item->id_produto}");
-
-      if ($response->retorno->status_processamento != '3'){
-        continue;
-      }
-
-      $components = (array) $response->retorno->produto;
+  }
+?>
 
 
-      uasort($components, 'compare');
+<?php
+function components($products = []){
+  $grouped_components = [];
 
+  foreach($products as $product){
+    $response = api_request('https://api.tiny.com.br/api2/produto.obter.estrutura.php', "id={$product->item->id_produto}");
 
-      foreach($components as $component) {
-        $component_total = $component->item->quantidade * $item->item->quantidade;
-
-        echo "<tr>";
-          echo "<td>{$component->item->nome}</td>";
-          echo "<td>{$component_total}</td>";
-        echo "</tr>";
-      }
+    if ($response->retorno->status_processamento != '3'){
+      continue;
     }
 
-    echo "</table>";
+    $components = (array) $response->retorno->produto;
+
+    uasort($components, 'compare');
+
+    foreach ($components as $component) {
+      if (!array_key_exists($component->item->nome, $grouped_components)){
+        $grouped_components[$component->item->nome] = 0;
+      }
+
+      $grouped_components[$component->item->nome] += $component->item->quantidade * $product->item->quantidade;
+    }
   }
+
+  $text = "";
+  foreach($grouped_components as $name => $total){
+    $text .= "
+      <tr>
+        <td>{$name}</td>
+        <td>{$total}</td>
+      </tr>
+    ";
+  }
+
+  return $text;
+}
 ?>
