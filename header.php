872a68a8c62a25aca5820534cc75c28cf6867b36
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2023-10-18 12:08:43 -0300

    Add auth to pages

--- header.php
+++ header.php
@@ -1,5 +1,14 @@
 <?php include 'functions.php';?>
 
+<?php
+  if (!isset($_SERVER['PHP_AUTH_USER']) || ($_SERVER['PHP_AUTH_USER'] !== 'user' || $_SERVER['PHP_AUTH_PW'] !== 'pass')) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+
+    exit;
+  }
+?>
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 <!DOCTYPE html>
 <html lang="pt-BR">
